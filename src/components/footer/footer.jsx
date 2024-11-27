c980623583b27152d52c8a6938fd4445c09e45fd
danula-ded <stupishin1597@yandex.ru>
2024-11-27 13:54:31 +0300

    feat: add NavLink

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './footer.css';
 
+import { NavLink } from "react-router";
+
 import { Button } from '@consta/uikit/Button';
 import { Layout } from '@consta/uikit/Layout';
 import { Text } from '@consta/uikit/Text';
@@ -10,12 +12,16 @@ const Footer = () => {
         <Layout className="footer">
             <Layout className="footer__nav">
                 <Layout className="footer__nav-buttons">
+                <NavLink to="/">
                     <Button size="l" label="Контакты" view="clear" />
+                </NavLink>
+                <NavLink to="/service">
                     <Button size="l" label="О нас" view="ghost" form="brickRound" />
+                </NavLink>
                 </Layout>
             </Layout>
             <Text size='xl' lineHeight="m" view="primary" className="footer__title">
-                @ Ступишин Даниил
+                © Ступишин Даниил
             </Text>
         </Layout>
     );
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./header.css";
 
+import { NavLink } from "react-router";
+
 import { Button } from '@consta/uikit/Button';
 import { Layout } from '@consta/uikit/Layout';
 import { Text } from '@consta/uikit/Text';
@@ -8,13 +10,19 @@ import { Text } from '@consta/uikit/Text';
 const Header = () => {
     return (
         <Layout className="header">
-            <Text size="2xl" lineHeight="m" view="primary" className="header__title">
-                Ступишин Даниил
-            </Text>
+            <NavLink to="https://github.com/danula-ded">
+                <Text size="2xl" lineHeight="m" view="primary" className="header__title">
+                    Ступишин Даниил
+                </Text>
+            </NavLink>
             <Layout className="header__nav">
                 <Layout className="header__nav-buttons">
-                    <Button size='l' label="Главная" view="ghost" form="roundBrick" />
-                    <Button size='l' label="Услуги" view="clear" />
+                    <NavLink to="/">
+                        <Button size='l' label="Главная" view="ghost" form="roundBrick" />
+                    </NavLink>
+                    <NavLink to="/service">
+                        <Button size='l' label="Услуги" view="clear" />
+                    </NavLink>
                 </Layout>
                 <Layout className="header__registration">
                     <Button size='l' label="Регистрация" view="clear" />
