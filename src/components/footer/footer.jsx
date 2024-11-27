938a0d7ceb09f67beb67cb147fa5857b26ed45fa
danula-ded <stupishin1597@yandex.ru>
2024-11-27 22:15:26 +0300

    feat: add highlighting tab when Route = Url

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -9,17 +9,27 @@ import { Button } from '@consta/uikit/Button';
 import { Layout } from '@consta/uikit/Layout';
 import { Text } from '@consta/uikit/Text';
 
+const getStyleForNavLink = ({ isActive }) =>
+    isActive
+        ? {
+            boxShadow: 'inset 0 0 10px rgba(0, 0, 0, 0.5)',
+            borderRadius: '20px',
+        }
+        : {}
+    ;
+
+
 const Footer = () => {
     return (
         <Layout className="footer">
             <Layout className="footer__nav">
                 <Layout className="footer__nav-buttons">
-                <NavLink to={AppRoute.main}>
-                    <Button size="l" label="Контакты" view="clear" />
-                </NavLink>
-                <NavLink to={AppRoute.service}>
-                    <Button size="l" label="О нас" view="ghost" form="brickRound" />
-                </NavLink>
+                    <NavLink to={AppRoute.main} style={getStyleForNavLink}>
+                        <Button size="l" label="Главная" view="clear" form="round" />
+                    </NavLink>
+                    <NavLink to={AppRoute.service} style={getStyleForNavLink}>
+                        <Button size="l" label="Услуги" view="clear" form="round" />
+                    </NavLink>
                 </Layout>
             </Layout>
             <Text size='xl' lineHeight="m" view="primary" className="footer__title">
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,6 +9,15 @@ import { Button } from '@consta/uikit/Button';
 import { Layout } from '@consta/uikit/Layout';
 import { Text } from '@consta/uikit/Text';
 
+const getStyleForNavLink = ({ isActive }) =>
+    isActive
+        ? {
+            boxShadow: 'inset 0 0 10px rgba(0, 0, 0, 0.5)',
+            borderRadius: '20px',
+        }
+        : {}
+    ;
+
 const Header = () => {
     return (
         <Layout className="header">
@@ -19,15 +28,15 @@ const Header = () => {
             </NavLink>
             <Layout className="header__nav">
                 <Layout className="header__nav-buttons">
-                    <NavLink to={AppRoute.main}>
-                        <Button size='l' label="Главная" view="ghost" form="roundBrick" />
+                    <NavLink to={AppRoute.main} style={getStyleForNavLink}>
+                        <Button size='l' label="Главная" view="clear" form="round" />
                     </NavLink>
-                    <NavLink to={AppRoute.service}>
-                        <Button size='l' label="Услуги" view="clear" />
+                    <NavLink to={AppRoute.service} style={getStyleForNavLink}>
+                        <Button size='l' label="Услуги" view="clear" form="round" />
                     </NavLink>
                 </Layout>
                 <Layout className="header__registration">
-                    <Button size='l' label="Регистрация" view="clear" />
+                    <Button size='l' label="Регистрация" view="clear" form="round" />
                 </Layout>
             </Layout>
         </Layout>
