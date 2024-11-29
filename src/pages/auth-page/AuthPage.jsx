300718997b4489d3c7e9d523a3e7156428eef438
danula-ded <stupishin1597@yandex.ru>
2024-11-29 13:54:40 +0300

    feat: validation form

--- src/pages/auth-page/AuthPage.jsx
+++ src/pages/auth-page/AuthPage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./AuthPage.css";
 import { Button } from "@consta/uikit/Button";
+import { Informer } from '@consta/uikit/Informer';
+// import IconThumbUp from '@consta/uikit/IconThumbUp';
+// import IconThumbDown from '@consta/uikit/IconThumbDown';
 
 const AuthPage = () => {
 
@@ -8,6 +11,7 @@ const AuthPage = () => {
         username: '',
         password: ''
     })
+    const [message, setMessage] = React.useState(null);
     const fieldChangeHandle = (evt) => {
         const { name, value } = evt.target;
         setFormData({
@@ -16,8 +20,22 @@ const AuthPage = () => {
         })
     }
     const formSubmitHandle = (evt) => {
-        alert(`Username: ${formData.username}\nPassword: ${formData.password}`);
-    }
+        evt.preventDefault();
+        if (formData.username.trim() && formData.password.trim()) {
+            setMessage({
+                label: "Все хорошо",
+                view: "filled",
+                status: "success",
+            });
+        } else {
+            setMessage({
+                label: "Заполните все поля",
+                view: "filled",
+                status: "alert",
+            });
+        }
+    };
+
 
     return (
         <>
@@ -28,8 +46,18 @@ const AuthPage = () => {
                     <input id="firstName" name="username" value={formData.username} onChange={fieldChangeHandle} />
                     <label htmlFor="password">Пароль:</label>
                     <input id="password" name="password" value={formData.password} onChange={fieldChangeHandle} />
+                    {message && (
+                        <Informer
+                            label={message.label}
+                            view={message.view}
+                            status={message.status}
+                            className="feedback-form-informer"
+                        />
+                    )}
                     <Button size='m' label="Вход" form="round" onClick={formSubmitHandle} />
                 </form>
+
+
             </div>
         </>
     );
